9d656f9a455884dc964705a97fd1f8855bd4fd84
Filipe Oliveira <filipeoliveirah@gmail.com>
2018-09-05 00:56:07 -0300

    Upload

--- forms/buscarassunto.php
+++ forms/buscarassunto.php
@@ -1,28 +1,33 @@
 <?php
-//https://jonsuh.com/blog/convert-loop-through-json-php-javascript-arrays-objects/ 
-//include_once('../conexao.php');
-$idDisciplinaSelecionada = $_POST["disciplinaSelecionada"];
-// Connect to the database
-  // replace the parameters with your proper credentials
-  $connection = mysqli_connect("localhost:8889", "root", "root", "concurso");
+  //https://jonsuh.com/blog/convert-loop-through-json-php-javascript-arrays-objects/ 
+  include_once('../conexao.php');
+  $idDisciplinaSelecionada = $_POST["disciplinaSelecionada"];
 
   // Query to run
-  $query = mysqli_query($connection, "SELECT * FROM assunto WHERE disciplina_idDiscipplina = '" . $idDisciplinaSelecionada . "'");
+  $buscarBanco = $pdo->prepare("SELECT * FROM assunto WHERE disciplina_idDiscipplina = ?");
+  $array_sql = array(
+    $idDisciplinaSelecionada
+  );
 
-  // Create empty array to hold query results
-  $someArray = [];
 
   // Loop through query and push results into $someArray;
-  while ($row = mysqli_fetch_assoc($query)) {
-    array_push($someArray, [
-      'name'   => $row['idAssunto'],
-      'gender' => $row['nomeAssunto']
-    ]);
-  }
 
+  if($buscarBanco->execute($array_sql)){
+    while ($stmt->fetch(PDO::FETCH_OBJ)){
+      array_push($someArray, [
+        'idAssunto'   => $row['idAssunto'],
+        'nomeAssunto' => $row['nomeAssunto']
+      ]);
+    }
+    echo json_encode($someArray);
+  }
+  else{
+    $respostas['erro'] = 'sim';
+    $respostas['getErro'] = 'Não foi possível encontrar um assunto.';
+    echo json_encode($respostas);
+  }
   // Convert the Array to a JSON String and echo it
-  $someJSON = json_encode($someArray);
-  echo $someJSON;
+
 
     //        $respostas['erro'] = 'sim';
     //$respostas['getErro'] = 'Não encontramos assuntos relacionados.';
--- index.php
+++ index.php
@@ -102,7 +102,8 @@
     </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <!---<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>-->
     <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -121,13 +122,13 @@
             $('.resp').html('<div class="alert alert-warning" role="alert"><p>Aguarde enquanto enviamos seu cadastro.</p></div>');
           }
         })
-        .done(function( valor ) {
+        .success(function( valor ) {
           if(valor.erro == 'sim'){
             $('.resp').html('<div class="alert alert-danger" role="alert"><p>'+valor.getErro+'</p></div>');
           }
           else{
             //FAZER UM APPED NO OPTION DO SELECT ASSUNTO 
-            $('.resp').html('<div class="alert alert-danger" role="alert"><p>'+valor.idAssunto+'</p></div>');
+            $('.resp').html('<div class="alert alert-danger" role="alert"><p>'+valor.nomeAssunto+'</p></div>');
           }
         })              
         .fail(function( jqXHR, errorThrown  ) {
